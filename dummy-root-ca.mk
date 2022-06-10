f9a4a19e28d7c18adcd1dc35253cf2a4e5a83b49
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2022-06-10 19:10:35 +0300

    some params

--- dummy-root-ca.mk
+++ dummy-root-ca.mk
@@ -1,12 +1,14 @@
 #!/usr/bin/make -f
 
-subjectAltName :=
+root.ca := $(USER)\'s Dummy Root CA
+tls.altname :=
+d := 3650
 
 all: $(shell hostname -f).crt
 
 root.crt: root.pem
-	openssl req -x509 -key $< -out $@ -days 3650 \
-	 -subj /CN="$$USER Test Root CA" \
+	openssl req -x509 -key $< -out $@ -days $(d) \
+	 -subj /CN="$(root.ca)" \
 	 -addext basicConstraints=critical,CA:TRUE,pathlen:0 \
 	 -addext keyUsage=critical,keyCertSign,cRLSign \
 	 -addext nsCertType=sslCA,emailCA,objCA
@@ -16,11 +18,11 @@ root.crt: root.pem
 
 %.crt: %.pem root.crt
 	openssl req -x509 -key $< -CA root.crt -CAkey root.pem -out $@ \
-	 -days 365 \
+	 -days $(d) \
 	 -subj /CN=$(basename $@) \
-	 -addext subjectAltName=$(or $(subjectAltName),$(mk_subjectAltName)) \
+	 -addext subjectAltName=$(or $(tls.altname),$(altname)) \
 	 -addext basicConstraints=critical,CA:FALSE \
 	 -addext extendedKeyUsage=serverAuth \
 	 -addext keyUsage=digitalSignature,nonRepudiation,keyEncipherment,keyAgreement
 
-mk_subjectAltName = DNS:$(basename $@),$(shell ip -br addr | awk '{split($$3, a, "/"); print "IP:"a[1]}' | paste -s -d,)
+altname = DNS:$(basename $@),$(shell ip -br addr | awk '{split($$3, a, "/"); print "IP:"a[1]}' | paste -s -d,)
