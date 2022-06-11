d2594774f1655b89fbbfa2d348af91f004f13be6
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2022-06-11 10:21:28 +0300

    on_CN_changed

--- dummy-root-ca.c
+++ dummy-root-ca.c
@@ -21,14 +21,52 @@ void quit() {
   gtk_main_quit();
 }
 
-void on_generate_clicked(GtkButton *w) {
+void on_out_button_clicked() {
+  GtkWindow *parent = GTK_WINDOW(gtk_builder_get_object(builder, "toplevel"));
+  GtkWidget *w = gtk_file_chooser_dialog_new(NULL, parent, GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER, "Cancel", GTK_RESPONSE_CANCEL, "Choose", GTK_RESPONSE_ACCEPT, NULL);
+  if (gtk_dialog_run(GTK_DIALOG(w)) == GTK_RESPONSE_ACCEPT) {
+    char *dir = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(w));
+    gtk_entry_set_text(GTK_ENTRY(gtk_builder_get_object(builder, "out")), dir);
+    g_free(dir);
+  }
+
+  gtk_widget_destroy(w);
+}
+
+void on_CN_changed(GtkEntry *w) {
+  const gchar *cn = gtk_entry_get_text(w);
+
+  // insert CN into 'files' table
+  GtkTreeView *tv = GTK_TREE_VIEW(gtk_builder_get_object(builder, "files"));
+  GtkListStore *ls = GTK_LIST_STORE(gtk_builder_get_object(builder, "files_ls"));
+  GtkTreeModel *model = gtk_tree_view_get_model(tv);
+  GtkTreeIter i;
+  char fname[BUFSIZ];
+
+  gtk_tree_model_get_iter_from_string(model, &i, "1");
+  snprintf(fname, sizeof fname, (strlen(cn) ? "%s.pem" : "%s-"), cn);
+  gtk_list_store_set(ls, &i, 1, fname, -1);
+  snprintf(fname, sizeof fname, (strlen(cn) ? "%s.crt" : "%s-"), cn);
+  gtk_list_store_set(ls, &i, 2, fname, -1);
+}
+
+void on_generate_clicked() {
   fprintf(stderr, "on_generate_clicked\n");
+
+  const gchar *out = gtk_entry_get_text(GTK_ENTRY(gtk_builder_get_object(builder, "out")));
+  fprintf(stderr, "out: %s\n", out);
 
   gint days = gtk_spin_button_get_value(GTK_SPIN_BUTTON(gtk_builder_get_object(builder, "days")));
   fprintf(stderr, "days: %d\n", days);
 
   gchar *key_size = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(gtk_builder_get_object(builder, "key_size")));
   fprintf(stderr, "key size: %s\n", key_size);
+
+  const gchar *cn = gtk_entry_get_text(GTK_ENTRY(gtk_builder_get_object(builder, "CN")));
+  fprintf(stderr, "CN: %s\n", cn);
+
+  const gchar *altname = gtk_entry_get_text(GTK_ENTRY(gtk_builder_get_object(builder, "subjectAltName")));
+  fprintf(stderr, "subjectAltName: %s\n", altname);
 
   gboolean overwrite_all = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(gtk_builder_get_object(builder, "overwrite1")));
   fprintf(stderr, "overwrite all: %d\n", overwrite_all);
